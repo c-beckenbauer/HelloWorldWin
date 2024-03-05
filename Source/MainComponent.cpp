366a858962101d0eba13b2e6076bc7f14f1e352b
Clemens Wannemacher <clemens.wannemacher@kongresskultur.com>
2024-03-05 18:29:17 +0100

    Changed Title

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -18,7 +18,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (48.0f));
     g.setColour (juce::Colours::red);
-    g.drawText ("Hello Clemens!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello Test Clemens!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
